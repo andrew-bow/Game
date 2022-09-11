e9db0b8e90ae3a5991fadb18920f717b43719b24
andrew-bow <belopukhov00@gmail.com>
2022-09-11 23:34:23 +0300

    added Controller

--- src/main/java/com/example/game/controller/MyGameController.java
+++ src/main/java/com/example/game/controller/MyGameController.java
@@ -1,25 +1,33 @@
 package com.example.game.controller;
 
 import com.example.game.model.ResponseModel;
+import com.example.game.model.User;
+import com.example.game.repository.UserRepository;
+import com.example.game.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/game")
 public class MyGameController {
-    @GetMapping("/easy")
-    public ResponseModel getEasyResult() {
 
-        return new ResponseModel();
+    private final GameService gameService;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public MyGameController(GameService gameService, UserRepository userRepository) {
+        this.gameService = gameService;
+        this.userRepository = userRepository;
     }
-    @GetMapping("/medium")
-    public ResponseModel getMediumResult() {
 
-        return new ResponseModel();
+    @GetMapping("/play/{userId}/{choice}")
+    public ResponseModel getResult(@PathVariable Integer userId , @PathVariable String choice) {
+        return gameService.getResult(choice, userId);
     }
-    @GetMapping("/hard")
-    public ResponseModel getHardResult() {
-
-        return new ResponseModel();
+    @GetMapping("/{userId}")
+    public User getUser(@PathVariable Integer userId) {
+        return userRepository.getUser(userId);
     }
 
 }
